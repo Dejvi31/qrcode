45cb6ed99f8095416dc130c91cd9e0ead3c02f9e
dejvi <dejviepadejvi@gmail.com>
2023-02-20 20:41:32 +0100

    shtuam projektin e mbaruar

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+import  QRCode from 'qrcode';
+import './index.css';
+
+const App = () => {
+  const [url,setUrl] = useState('')
+  const [qrcode,setQrcode] = useState('')
+
+  const GenerateQRCode = () => {
+   QRCode.toDataURL(url, {
+    width: 800,
+    margin: 2,
+    color: {
+      dark: '#000000ff',
+      light: '#ffffffff'
+    }
+   },(err,url) => {
+      if(err) return console.error(err)
+      console.log(url)
+      setQrcode(url)
+    })
+  }
+  return (
+    <div className='app'>
+      <h1>QR Code Generator</h1>
+      <input 
+       type='text'
+       placeholder='Add your link ...'
+      value={url}
+      onChange={(e) => setUrl(e.target.value)}
+      />
+      <button onClick={GenerateQRCode}>Generate</button>
+      {qrcode && <>
+        <img src={qrcode} alt='qrcode'/>
+        <a href={qrcode} download="qrcode.png">Download</a>
+      </>}
+      
+    </div>
+  )
+}
+
+export default App
